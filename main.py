17b5183bc9f3d0bdbdce4d1974d5607e952a2fc5
zhanzhaozzz <96629146+zhanzhaozzz@users.noreply.github.com>
2022-09-04 14:49:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -308,6 +308,6 @@ if __name__ == "__main__":
     chp = get_tianhang()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en, max_temp, min_temp, sunrise,
+        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch,love_days, note_en, max_temp, min_temp, sunrise,
                      sunset, category, pm2p5, proposal, chp)
     os.system("pause")
